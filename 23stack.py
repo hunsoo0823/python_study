e9c470ea9ff77aeb594eb511865174b5e5cdbd9b
hunsoo0823 <hunsoo0823@naver.com>
2020-11-28 23:55:28 +0900

    second time

--- 23stack.py
+++ 23stack.py
@@ -1,7 +1,5 @@
 stack = []
 
-# 삽입(5-2-3-7) - 삭제() - 삽입(1-4) - 삭제()
-
 stack.append(5)
 stack.append(2)
 stack.append(3)
@@ -11,5 +9,6 @@ stack.append(1)
 stack.append(4)
 stack.pop()
 
-print(stack) # 최하단 원소부터 출력
-print(stack[::-1]) # 최상단 원소부터 출
+print(stack)
+print(stack[::-1])
+
--- 24queue.py
+++ 24queue.py
@@ -1,3 +1,5 @@
+
+"""
 from collections import deque
 #큐 구현을 위해 deque라이브러리 사용
 
@@ -14,5 +16,23 @@ queue.append(4)
 queue.popleft()
 
 print(queue)#먼저들어온 순서대로 출력
+queue.reverse()
+print(queue)
+"""
+
+from collections import deque
+
+queue = deque()
+
+queue.append(5)
+queue.append(2)
+queue.append(3)
+queue.append(7)
+queue.popleft()
+queue.append(1)
+queue.append(4)
+queue.popleft()
+
+print(queue)
 queue.reverse()
 print(queue)
