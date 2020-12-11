82d928a11b2d589dbe90f6cfb0bc33f3a6cf27a7
hunsoo0823 <hunsoo0823@naver.com>
2020-12-11 23:49:01 +0900

    second

--- 41fibonacci_recursion.py
+++ 41fibonacci_recursion.py
@@ -1,7 +1,8 @@
+"""
 # 한 번 계산된 결과를 메모이제이션(Memoization)하기 위한 리스트 초기화
 d = [0] * 100
 
-# 피보나치 함수(Fibonacci Function)를 재귀ㅎ마수로 구현(탑다운 다이나믹 프로그래밍)
+# 피보나치 함수(Fibonacci Function)를 재귀함수로 구현(탑다운 다이나믹 프로그래밍)
 def fibo(x):
     # 종료조건 x == 1 or x == 2
     if x == 1 or x == 2:
@@ -14,3 +15,19 @@ def fibo(x):
     return d[x]
 
 print(fibo(99))
+"""
+
+d = [0] * 100
+
+def fibo(x):
+    if x == 1 or x == 2:
+        return 1
+    if d[x] != 0:
+        return d[x]
+
+    d[x] = fibo(x-1) + fibo(x-2)
+    return d[x]
+
+print(fibo(99))
+
+
--- 42fibonacci_iteration.py
+++ 42fibonacci_iteration.py
@@ -1,3 +1,4 @@
+"""
 d = [0] * 100
 
 def fibo(x):
@@ -20,6 +21,18 @@ d[2] = 1
 n = 99
 
 # 피보나치 함수(Fibonacci Function) 반복문으로 구현(보텀업 다이나믹 프로그래밍)
+for i in range(3, n+1):
+    d[i] = d[i-1] + d[i-2]
+
+print(d[n])
+"""
+
+d = [0] * 100
+
+d[1] = 1
+d[2] = 1
+n = 99
+
 for i in range(3, n+1):
     d[i] = d[i-1] + d[i-2]
 
