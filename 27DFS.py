14ba90f5bec2b6dfbcff95bbbfb6347c0a751efc
hunsoo0823 <hunsoo0823@naver.com>
2021-04-23 21:55:59 +0900

    DFS, BFS

--- 27DFS.py
+++ 27DFS.py
@@ -25,25 +25,31 @@ visted = [False] * 9
 dfs(graph, 1, visted)
 """
 
-def dfs(graph, v, visted):
-    visted[v] = True
-    print(v,end=' ')
+# DFS 메서드 정의
+
+def dfs(graph, v, visited):
+    # 현재 노드를 방문 처리
+    visited[v] = True
+    print(v, end=' ')
+    # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
     for i in graph[v]:
-        if not visted[i]:
+        if not visited[i]:
             dfs(graph, i, visted)
 
 graph = [
     [],
-    [2,3,8],
-    [1,7],
-    [1,4,5],
-    [3,5],
-    [3,4],
+    [2, 3, 8],
+    [1, 7],
+    [4, 5],
+    [3, 5],
+    [3, 4],
     [7],
-    [2,6,8],
-    [1,7]
+    [2, 6, 8],
+    [1, 7]
 ]
 
 visted = [False] * 9
 
-dfs(graph,1,visted)
+dfs(graph, 1, visted)
+
+
--- 28BFS.py
+++ 28BFS.py
@@ -38,25 +38,31 @@ visted = [False] * 9
 # 정의된 BFS 함수 호출
 bfs(graph, 1, visted)
 """
+
 from collections import deque
 
+# BFS 메서드 정의
 def bfs(graph, start, visted):
+    # 큐(Queue) 구현을 위해 deque 라이브러리 사용
     queue = deque([start])
+    # 현재 노드를 방문 처리
     visted[start] = True
-
+    # 큐가 빌 때까지 반복
     while queue:
+        # 큐에서 하나의 원소를 뽑아 출력
         v = queue.popleft()
         print(v, end=' ')
+        # 해당 원소와 연결된, 아직 방문되지 않은 원소들을 큐에 삽입
         for i in graph[v]:
             if not visted[i]:
                 queue.append(i)
                 visted[i] = True
-                
+
 graph = [
     [],
     [2, 3, 8],
     [1, 7],
-    [1, 4, 5],
+    [4, 5],
     [3, 5],
     [3, 4],
     [7],
@@ -64,5 +70,6 @@ graph = [
     [1, 7]
 ]
 
-visited = [False] * 9
-bfs(graph, 1, visited)
+visted = [False] * 9
+
+bfs(graph, 1, visted)
